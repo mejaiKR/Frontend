aaa5a7ee060e3d830b3ea83c5731a76688248912
donghyun <donghyun1998@naver.com>
2024-03-20 19:37:07 +0900

    feat: mod shadcn skeleton bg

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -6,7 +6,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-muted dark:bg-gray-950",
+        className,
+      )}
       {...props}
     />
   );
