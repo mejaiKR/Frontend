0e67fc3dd800d9f5d8a56c5937a073752fd3f6fb
donghyun <donghyun1998@naver.com>
2024-03-20 15:34:25 +0900

    feat: 월, 요일 바 일단 주석

--- src/app/summoner-page/jandi.tsx
+++ src/app/summoner-page/jandi.tsx
@@ -7,7 +7,7 @@ export default function Jandi() {
     <div className="flex m-6 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       {/* <OneWeekBar /> */}
       <div className="flex flex-col overflow-x-auto">
-        <MonthBar />
+        {/* <MonthBar /> */}
         <div className="grid grid-flow-col auto-cols-max grid-rows-7">
           {Array.from({ length: 365 }).map((_, index) => (
             <Mejais key={index} index={index} />
