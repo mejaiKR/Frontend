c50069cc6246806d22bda84710fa2bc0948aa0a3
donghyun <donghyun1998@naver.com>
2024-03-27 20:00:07 +0900

    feat: dayjs 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import MainSearchBar from "@/app/mainSearchBar";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="font-[S-SBI] scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl mb-6">
+    <div className="w-full flex flex-col items-center justify-center">
+      <h1 className="mt-28 font-[S-SBI] scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl mb-6">
         Mejai.GG
       </h1>
       <blockquote className="text-xl italic font-semibold text-gray-900 dark:text-white mb-6">
