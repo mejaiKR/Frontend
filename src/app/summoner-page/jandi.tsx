fef5af826d2678601867990c6d38a9393e74c3d8
donghyun <donghyun1998@naver.com>
2024-03-27 19:57:30 +0900

    feat: 목업 코드 추가

--- src/app/summoner-page/jandi.tsx
+++ src/app/summoner-page/jandi.tsx
@@ -1,16 +1,44 @@
-import Mejais from "@/app/summoner-page/mejais";
-import MonthBar from "@/app/summoner-page/monthBar";
-import OneWeekBar from "@/app/summoner-page/oneWeekBar";
-
+import MejaiBox from "@/app/summoner-page/mejai-box";
+import dayjs from "dayjs";
+// interface MatchData {
+//     [date: string]: number;
+// }
+// async function fetchMatchData() {
+//     let mockup = {
+//         "2024-01-02" : 3,
+//         "2024-01-03" : 2,
+//         "2024-01-06" : 3,
+//         "2024-01-08" : 3,
+//     }
+//     return mockup;
+// }
+let mockup = {
+  "2024-01-02": 3,
+  "2024-01-03": 2,
+  "2024-01-06": 3,
+  "2024-01-08": 3,
+};
 export default function Jandi() {
+  const startOfDay = dayjs("2024-01-01");
+  const endOfDay = dayjs("2024-12-31");
+  let currentDate = startOfDay;
+  const dateMap = new Map<string, number>();
+
+  // while (
+  //   currentDate.isBefore(endOfDay) ||
+  //   currentDate.isSame(endOfDay, "day")
+  // ) {
+  //   const dateString = currentDate.format("YYYY-MM-DD");
+  //   dateMap.set(dateString, numberForDate);
+  //   currentDate = currentDate.add(1, "day");
+  // }
   return (
     <div className="flex m-6 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      {/* <OneWeekBar /> */}
       <div className="flex flex-col overflow-x-auto">
-        {/* <MonthBar /> */}
         <div className="grid grid-flow-col auto-cols-max grid-rows-7">
+          {/*여기서 fetch해서 받은 object array로 .map돌려야겠다*/}
           {Array.from({ length: 365 }).map((_, index) => (
-            <Mejais key={index} index={index} />
+            <MejaiBox key={index} index={index} />
           ))}
         </div>
       </div>
