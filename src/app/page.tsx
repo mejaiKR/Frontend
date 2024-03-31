0032a581af51f238a09f57d9fb675b9362c8ef4e
donghyun <donghyun1998@naver.com>
2024-03-31 16:20:15 +0900

    feat: font 이쁜거로 바굼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MainSearchBar from "@/app/mainSearchBar";
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="mt-28 font-[S-SBI] scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl mb-6">
+      <h1 className="mt-28 font-[BFL-B] scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl mb-6">
         Mejai.GG
       </h1>
       <blockquote className="text-xl italic font-semibold text-gray-900 dark:text-white mb-6">
