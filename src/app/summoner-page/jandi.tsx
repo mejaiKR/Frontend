4cf58084ad63dd433b1a74d695e5bfb31179b888
donghyun <donghyun1998@naver.com>
2024-03-31 16:20:46 +0900

    feat: reactquery 관련 주석 추가 이제 풀리퀘로 한다!

--- src/app/summoner-page/jandi.tsx
+++ src/app/summoner-page/jandi.tsx
@@ -1,5 +1,6 @@
 import MejaiBox from "@/app/summoner-page/mejai-box";
 import dayjs from "dayjs";
+import { Button } from "@/components/ui/button";
 // interface MatchData {
 //     [date: string]: number;
 // }
@@ -19,10 +20,10 @@ let mockup = {
   "2024-01-08": 3,
 };
 export default function Jandi() {
+  // 멀티서치 적용할거면 여기다가 props로 이름 받아서 fetch해야겠다
   const startOfDay = dayjs("2024-01-01");
   const endOfDay = dayjs("2024-12-31");
   let currentDate = startOfDay;
-  const dateMap = new Map<string, number>();
 
   // while (
   //   currentDate.isBefore(endOfDay) ||
@@ -35,6 +36,7 @@ export default function Jandi() {
   return (
     <div className="flex m-6 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div className="flex flex-col overflow-x-auto">
+        {/*여기에 랭크 일반 토글박스 추가계획*/}
         <div className="grid grid-flow-col auto-cols-max grid-rows-7">
           {/*여기서 fetch해서 받은 object array로 .map돌려야겠다*/}
           {Array.from({ length: 365 }).map((_, index) => (
--- src/app/summoner-page/page.tsx
+++ src/app/summoner-page/page.tsx
@@ -3,7 +3,6 @@ import Jandi from "@/app/summoner-page/jandi";
 import UserInfoBox from "@/app/summoner-page/user-info-box";
 
 export default function SummonerPage() {
-  // react query 사용해서 동적으로 렌더링 해주자
   return (
     <>
       <UserInfoBox />
