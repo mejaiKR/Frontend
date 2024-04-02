7434e948f173c9f336070329f7db371d7c8e5f1a
donghyun <donghyun1998@naver.com>
2024-04-02 15:08:50 +0900

    feat: react-query provider 클라이언트컴포넌트로 만듬

---
+++ src/components/reactQueryProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useState } from "react";
+
+export default function reactQueryProvider({ children }) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 refetch
+          // refetchOnMount: false, // 데이터가 stale 상태이면 컴포넌트가 마운트될 때 refetch
+          retry: 1, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+        },
+      },
+    }),
+  );
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
