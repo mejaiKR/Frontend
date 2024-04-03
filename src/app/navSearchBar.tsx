0789db69f5f43230036975e1f10f3ecb158b9231
donghyun <donghyun1998@naver.com>
2024-04-03 14:06:09 +0900

    feat: 검색 창 최대길이 22 (닉네임 16 # 태그 5)로 설정

--- src/app/navSearchBar.tsx
+++ src/app/navSearchBar.tsx
@@ -35,6 +35,7 @@ export default function NavSearchBar() {
         <input
           type="search"
           id="default-search"
+          maxLength={22}
           ref={inputRef}
           className="block w-full p-2 ps-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="소환사명#태그"
