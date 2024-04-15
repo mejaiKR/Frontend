1b67b770e44cdd5215445c921b1b1b429e3a6a35
donghyun <donghyun1998@naver.com>
2024-04-15 16:36:27 +0900

    feat: 다크모드 변경버튼 리스트없애고 버튼클릭으로 변경

--- src/app/modeToggle.tsx
+++ src/app/modeToggle.tsx
@@ -1,40 +1,44 @@
-'use client'
-
-import * as React from 'react'
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
-import { useTheme } from 'next-themes'
-
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+"use client";
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  // 초기 상태를 'system'으로 설정하고, 유효한 값이 있는지 확인
+  const [currentTheme, setCurrentTheme] = React.useState<
+    "light" | "dark" | "system"
+  >("system");
+
+  React.useEffect(() => {
+    // 'theme'와 'resolvedTheme'가 유효한 값인지 확인하고 상태 업데이트
+    const effectiveTheme = theme === "system" ? resolvedTheme : theme;
+    if (
+      effectiveTheme === "light" ||
+      effectiveTheme === "dark" ||
+      effectiveTheme === "system"
+    ) {
+      setCurrentTheme(effectiveTheme);
+    }
+  }, [theme, resolvedTheme]);
+
+  const toggleTheme = () => {
+    // 현재 테마에 따라 테마 변경
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    setCurrentTheme(newTheme);
+    setTheme(newTheme);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {currentTheme === "dark" ? (
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+      ) : (
+        <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
