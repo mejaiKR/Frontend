40e2b7c9da5dc606c17b529745a49db5096f4b1d
donghyun <donghyun1998@naver.com>
2024-04-27 02:17:38 +0900

    feat: 직접 넣어야 하나?

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-import {SERVER_URL} from "./src/lib/utils";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [SERVER_URL, 'localhost'], // 로컬호스트 도메인 임시로 추가
+		domains: [process.env.NEXT_PUBLIC_SERVER_URL, 'localhost'], // 로컬호스트 도메인 임시로 추가
 	},
 }
 export default nextConfig
