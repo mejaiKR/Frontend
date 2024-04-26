0eac4af035b45776473680c061056d1d89a2e2a0
donghyun <donghyun1998@naver.com>
2024-04-27 02:15:46 +0900

    feat: domain 출처 서버url도 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+import {SERVER_URL} from "./src/lib/utils";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['localhost'], // 로컬호스트 도메인 임시로 추가
+		domains: [SERVER_URL, 'localhost'], // 로컬호스트 도메인 임시로 추가
 	},
 }
 export default nextConfig
