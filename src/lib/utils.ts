b431a1c578843cadb84b30095e6c56e067206efa
donghyun <donghyun1998@naver.com>
2024-04-27 01:13:40 +0900

    feat: nextjs에선 .env 앞에 NEXT_PUBLIC_ 붙혀야하네요

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const SERVER_URL = process.env.SERVER_URL;
-export const S3_URL = process.env.S3_URL;
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const S3_URL = process.env.NEXT_PUBLIC_S3_URL;
