b5227b99cb6f2bdc13a0e5c14e5114f7ece9f25c
donghyun <donghyun1998@naver.com>
2024-04-27 02:23:48 +0900

    feat: ${}를 넣어야 하는군요...

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [process.env.NEXT_PUBLIC_SERVER_URL, 'localhost'], // 로컬호스트 도메인 임시로 추가
+		domains: [`${process.env.NEXT_PUBLIC_SERVER_URL}`,`${process.env.NEXT_PUBLIC_S3_URL}`, 'localhost'], // 로컬호스트 도메인 임시로 추가
 	},
 }
 export default nextConfig
