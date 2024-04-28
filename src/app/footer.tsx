b76e6c80e81b186183b535b8a668345c1f8c3e65
donghyun <donghyun1998@naver.com>
2024-04-28 20:39:15 +0900

    feat: footer github 주소 변경

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -5,10 +5,7 @@ export default function Footer() {
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          <a
-            href="https://github.com/orgs/mejaiGG/repositories"
-            className="hover:underline"
-          >
+          <a href="https://github.com/orgs/mejaiKR" className="hover:underline">
             github
           </a>
         </span>
