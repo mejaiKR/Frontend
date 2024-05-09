44c88c79402475cd8dfa2f11763b2136fa6d0040
donghyun <donghyun1998@naver.com>
2024-05-09 16:51:46 +0900

    chore: fontsize 이거 왜 sm으로 되어있지? 아놔

--- src/app/searchHistoryBox.tsx
+++ src/app/searchHistoryBox.tsx
@@ -17,7 +17,7 @@ export default function SearchHistoryBox() {
   return (
     <ScrollArea className="h-29 w-full rounded-md border">
       <div className="p-4">
-        <h4 className="mb-4 flex justify-center text-sm font-medium leading-none">
+        <h4 className="mb-4 flex justify-center font-medium leading-none">
           검색기록
         </h4>
         {searchHistoryArr.map((searchHistoryId) => {
