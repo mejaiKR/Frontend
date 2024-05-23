fae255cf6ee960e50d4bc40abcf6e9f3823574b7
donghyun <donghyun1998@naver.com>
2024-05-23 23:36:23 +0900

    feat: searchParam 지우고 ssr 가즈악

--- src/app/summoner-page/page.tsx
+++ src/app/summoner-page/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React from "react";
 import UserInfoBox from "@/app/summoner-page/userInfoBox";
 import TierBox from "@/app/summoner-page/tierBox";
-import { useSearchParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchUserInfo } from "@/app/summoner-page/fetchFunc";
 import ErrorPage from "@/app/summoner-page/errorPage";
@@ -11,8 +10,12 @@ import JandiBox from "@/app/summoner-page/jandiBox";
 import Spinner from "@/components/ui/spinner";
 import { AxiosError } from "axios";
 
-function AwaitPage() {
-  const params = useSearchParams();
+export default function SummonerPage({
+  searchParams,
+}: {
+  searchParams: URLSearchParams;
+}) {
+  const params = new URLSearchParams(searchParams);
   const id = params.get("id") || "";
   const tag = params.get("tag") || "";
   const { error, isLoading } = useQuery({
@@ -21,7 +24,6 @@ function AwaitPage() {
     staleTime: 1000 * 60 * 15, // 15분으로 staletime 설정
     gcTime: 1000 * 60 * 15,
   });
-
   if (error) {
     return <ErrorPage error={error as AxiosError} />;
   }
@@ -38,12 +40,5 @@ function AwaitPage() {
       <TierBox id={id} tag={tag} />
       <JandiBox />
     </>
-  );
-}
-export default function SummonerPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <AwaitPage />
-    </Suspense>
   );
 }
