06e80198244e5b9a681ef2d8d82d95c9dadb9fb3
kimchijinju <hanbimkim@gmail.com>
2024-07-18 11:44:22 +0900

    chore: ci/cd 설정

---
+++ scripts/start_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/ec2-user/your-app
+
+# 패키지 설치 및 빌드
+npm install
+npm run build
+
+# 현재 실행중인 next.js 서버가 있다면  kill 하고 서버 새로 시작
+PID=$(ps aux | grep 'next start -p 3000' | grep -v grep | awk '{print $2}')
+if [ -n "$PID" ]; then
+  kill -9 $PID
+fi
+
+nohup npm run start &
---
+++ scripts/validate_service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -I http://localhost:3000
