77751010af2ce995b3568ae83a7ab4eed91ad325
kimchijinju <hanbimkim@gmail.com>
2024-07-21 03:18:50 +0900

    chore: cicd script 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/build
 
 # 패키지 설치 및 빌드
 npm install
 npm run build
 
-# 현재 실행중인 next.js 서버가 있다면  kill 하고 서버 새로 시작
-PID=$(ps aux | grep 'next start -p 3000' | grep -v grep | awk '{print $2}')
-if [ -n "$PID" ]; then
-  kill -9 $PID
-fi
-
-nohup npm run start &
+# Start the application using PM2
+pm2 start "npm run start" --name mejai
+pm2 save
