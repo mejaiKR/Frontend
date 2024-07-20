f81cd21b158084c2abe5602839b8cd697113291a
kimchijinju <hanbimkim@gmail.com>
2024-07-21 01:31:14 +0900

    fix: npm 경로 강제추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/build
 
 # 패키지 설치 및 빌드
