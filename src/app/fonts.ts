0dfa8b263e07208c80f0c82ac8214d54eaa82c72
donghyun <donghyun1998@naver.com>
2024-08-03 13:11:41 +0900

    feat: preload 문제인가...?

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,16 +4,19 @@ export const gmarketMedium = localFont({
   src: "./../../public/fonts/GmarketSansTTFMedium.ttf",
   variable: "--font-gmarket-medium",
   display: "swap",
+  preload: true,
 });
 
 export const gmarketBold = localFont({
   src: "./../../public/fonts/GmarketSansTTFBold.ttf",
   variable: "--font-gmarket-bold",
   display: "swap",
+  preload: true,
 });
 
 export const netmarbleBold = localFont({
   src: "./../../public/fonts/netmarbleB.ttf",
   variable: "--font-netmarble-bold",
   display: "swap",
+  preload: true,
 });
