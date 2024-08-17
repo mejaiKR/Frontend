ca16ab8188ddc65e5a0b282d4e8946a3ddec4d58
donghyun <donghyun1998@naver.com>
2024-08-17 16:30:11 +0900

    feat: 디자이너행님의 의견을 따라 메인배너 빼버리기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="mt-28 mb-28 font-gmarketBold text-5xl tracking-tight lg:text-7xl">
-        mejai.kr
-      </h1>
-    </div>
-  );
+  return <></>;
 }
