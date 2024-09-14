f12e0215172ce3e6c14469dff576788c1682b3a9
donghyun <donghyun1998@naver.com>
2024-09-14 19:26:59 +0900

    feat: dev환경 cors우회용 프록시 설정

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,16 @@ const nextConfig = {
 	images: {
 		domains: [`${process.env.NEXT_PUBLIC_SERVER_URL}`,`${process.env.NEXT_PUBLIC_S3_URL}`, 'localhost'], // 로컬호스트 도메인 임시로 추가
 	},
+	async rewrites() {
+		if (process.env.NODE_ENV !== "development") {
+			return [];
+		}
+		return [
+			{
+				source: "/api/:path*",
+				destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,
+			},
+		];
+	},
 }
 export default nextConfig
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const SERVER_URL =
+  process.env.NODE_ENV !== "development"
+    ? process.env.NEXT_PUBLIC_SERVER_URL
+    : process.env.NEXT_PUBLIC_DEV_SERVER_URL;
 export const S3_URL = process.env.NEXT_PUBLIC_S3_URL;
