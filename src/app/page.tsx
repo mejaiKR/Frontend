aa6f9fd38ab385fb435faeac58643c34a8572547
donghyun <donghyun1998@naver.com>
2024-11-06 22:02:53 +0900

    feature: 움직유미로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-46 w-full flex justify-center items-center py-10">
-      <Image src="/yumi-banner.png" alt="banner" width={200} height={300} />
+      <Image src="/moving-yumi.gif" alt="banner" width={200} height={300} />
     </div>
   );
 }
