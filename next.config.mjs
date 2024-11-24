5126f047bb38b9e43b478fe0f147c9637e15ee26
donghyun <donghyun1998@naver.com>
2024-11-24 22:12:27 +0900

    feat: lint rule 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: [`${process.env.NEXT_PUBLIC_SERVER_URL}`,`${process.env.NEXT_PUBLIC_S3_URL}`, `${process.env.NEXT_PUBLIC_S3_DOMAIN}`],
-	},
-	async rewrites() {
-		if (process.env.NODE_ENV !== "development") {
-			return [];
-		}
-		return [
-			{
-				source: "/proxy/:path*",
-				destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,
-			},
-		];
-	},
-}
-export default nextConfig
+  images: {
+    domains: [
+      `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+      `${process.env.NEXT_PUBLIC_S3_URL}`,
+      `${process.env.NEXT_PUBLIC_S3_DOMAIN}`
+    ]
+  },
+  async rewrites() {
+    if (process.env.NODE_ENV !== "development") {
+      return [];
+    }
+    return [
+      {
+        source: "/proxy/:path*",
+        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`
+      }
+    ];
+  }
+};
+export default nextConfig;
