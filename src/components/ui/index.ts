46956302afd142c14d919aef00ffc4099090b195
donghyun <donghyun1998@naver.com>
2024-12-10 18:59:50 +0900

    feat: import chart ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./carousel";
+export * from "./chart";
 export * from "./command";
 export * from "./dialog";
 export * from "./drawer";
