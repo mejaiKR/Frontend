f2e3f5e3e1e4b31a54f0ec8a94c8162d5c97738c
donghyun <donghyun1998@naver.com>
2024-12-13 02:00:30 +0900

    style: tierbox skeleton design sync

--- src/app/summoner-page/_components/tier-box/tier-box.tsx
+++ src/app/summoner-page/_components/tier-box/tier-box.tsx
@@ -2,11 +2,10 @@
 
 import { nanoid } from "nanoid";
 
-import { ImageSkeleton } from "@/components/ui";
 import { useUserInfoQuery } from "@/queries";
 
+import { TierBoxSkeleton } from "./tier-box-skeleton";
 import { TierUnit } from "./tier-unit";
-
 type Props = {
   id: string;
   tag: string;
@@ -17,14 +16,13 @@ export const TierBox = ({ id, tag }: Props) => {
 
   if (isLoading || data === undefined)
     return (
-      <div className="m-6 flex h-32 rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800">
-        <ImageSkeleton />
-        <div className="ml-4 flex h-full w-60 flex-col justify-center"></div>
+      <div className="m-4 flex h-[208px] rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800">
+        <TierBoxSkeleton />
       </div>
     );
   if (error) {
     return (
-      <div className="m-6 flex h-32 items-center justify-center rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800">
+      <div className="m-4 flex rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800">
         <span className="font-netmarbleBold text-lg">error</span>
       </div>
     );
