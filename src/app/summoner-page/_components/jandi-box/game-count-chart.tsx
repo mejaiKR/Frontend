06466a35f9fbab4365d32dbfb09bd4daa526cc32
donghyun <donghyun1998@naver.com>
2024-12-18 16:31:45 +0900

    feat: 사용량에 적합한 라인차트로 변경

--- src/app/summoner-page/_components/jandi-box/game-count-chart.tsx
+++ src/app/summoner-page/_components/jandi-box/game-count-chart.tsx
@@ -1,30 +1,33 @@
 "use client";
 
-import { Label, Pie, PieChart } from "recharts";
-
 import {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import { ChartContainer, ChartTooltipContent } from "@/components/ui/chart";
 import { DayGameData } from "@/types";
 
 import { chartConfig } from "./const/chartConfig";
 
 type Props = {
   data: DayGameData[];
-  sumOfGameCount: number;
 };
 
-export const GameCountChart = ({ data, sumOfGameCount }: Props) => {
+export const GameCountChart = ({ data }: Props) => {
   const chartData = [
-    { day: "월요일", gameCount: 0, fill: "hsl(var(--chart-1))" },
-    { day: "화요일", gameCount: 0, fill: "hsl(var(--chart-2))" },
-    { day: "수요일", gameCount: 0, fill: "hsl(var(--chart-3))" },
-    { day: "목요일", gameCount: 0, fill: "hsl(var(--chart-4))" },
-    { day: "금요일", gameCount: 0, fill: "hsl(var(--chart-5))" },
-    { day: "토요일", gameCount: 0, fill: "hsl(var(--chart-6))" },
-    { day: "일요일", gameCount: 0, fill: "hsl(var(--chart-7))" },
+    { day: "월", gameCount: 0, fill: "hsl(var(--chart-1))" },
+    { day: "화", gameCount: 0, fill: "hsl(var(--chart-2))" },
+    { day: "수", gameCount: 0, fill: "hsl(var(--chart-3))" },
+    { day: "목", gameCount: 0, fill: "hsl(var(--chart-4))" },
+    { day: "금", gameCount: 0, fill: "hsl(var(--chart-5))" },
+    { day: "토", gameCount: 0, fill: "hsl(var(--chart-6))" },
+    { day: "일", gameCount: 0, fill: "hsl(var(--chart-7))" },
   ];
 
   data.forEach((game) => {
@@ -32,61 +35,28 @@ export const GameCountChart = ({ data, sumOfGameCount }: Props) => {
     chartData[dayIndex].gameCount += game.gameCount;
   });
 
-  chartData.sort((a, b) => {
-    return b.gameCount - a.gameCount;
-  });
-
   return (
-    <div className="mx-auto w-full max-w-md">
+    <div className="mx-auto mt-8 w-full max-w-md">
       <ChartContainer
         config={chartConfig}
         className="aspect-square h-[300px] w-full"
       >
-        <PieChart width={300} height={300}>
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent hideLabel />}
-          />
-          <Pie
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart
             data={chartData}
-            dataKey="gameCount"
-            nameKey="day"
-            innerRadius={60}
-            outerRadius={100}
-            strokeWidth={5}
-            isAnimationActive={false}
+            margin={{ top: 0, right: 20, bottom: 0, left: 0 }}
           >
-            <Label
-              content={({ viewBox }) => {
-                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                  return (
-                    <text
-                      x={viewBox.cx}
-                      y={viewBox.cy}
-                      textAnchor="middle"
-                      dominantBaseline="middle"
-                    >
-                      <tspan
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        className="fill-foreground text-3xl font-bold"
-                      >
-                        {sumOfGameCount}
-                      </tspan>
-                      <tspan
-                        x={viewBox.cx}
-                        y={(viewBox.cy ?? 0) + 24}
-                        className="fill-muted-foreground"
-                      >
-                        게임
-                      </tspan>
-                    </text>
-                  );
-                }
-              }}
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="day" />
+            <YAxis width={30} />
+            <Tooltip content={<ChartTooltipContent />} />
+            <Bar
+              dataKey="gameCount"
+              fill="hsl(var(--primary))"
+              radius={[4, 4, 0, 0]}
             />
-          </Pie>
-        </PieChart>
+          </BarChart>
+        </ResponsiveContainer>
       </ChartContainer>
     </div>
   );
