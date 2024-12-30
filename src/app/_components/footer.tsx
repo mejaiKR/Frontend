78fd437cfc3db798ac15a9be375d4b1ca448dd56
donghyun <donghyun1998@naver.com>
2024-12-31 01:18:26 +0900

    feat: 버그리포트링크 footer에 추가

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-
 export const Footer = () => {
   return (
     <footer className="m-4 rounded-lg bg-white shadow dark:bg-gray-800">
       <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        <span className="flex w-full justify-between text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+          <a
+            href="https://forms.gle/4gXMzTzjA2C9wb99A"
+            className="hover:underline"
+          >
+            버그 제보
+          </a>
           <a href="https://github.com/orgs/mejaiKR" className="hover:underline">
             github
           </a>
