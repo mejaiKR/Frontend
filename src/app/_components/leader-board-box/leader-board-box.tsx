662b55fc3ef583c1d6496d7724ca81d5e8cd0192
donghyun kim <donghyun1998@naver.com>
2025-01-01 11:54:28 +0900

    [Bugfix]: 새해 버그 수정 (#81)
    * fix: 1월일때 지난달 0월되는 이슈 수정
    
    * fix: lastUpdatedAt date 마이크로초 포맷으로 수정

--- src/app/_components/leader-board-box/leader-board-box.tsx
+++ src/app/_components/leader-board-box/leader-board-box.tsx
@@ -6,6 +6,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export const LeaderBoardBox = () => {
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
+  const lastMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+  const lastYear = currentMonth === 1 ? currentYear - 1 : currentYear;
 
   return (
     <Tabs defaultValue="currentMonth" className="w-full pt-8">
@@ -17,7 +19,7 @@ export const LeaderBoardBox = () => {
         <LeaderBoardUnit year={currentYear} month={currentMonth} />
       </TabsContent>
       <TabsContent value="lastMonth">
-        <LeaderBoardUnit year={currentYear} month={currentMonth - 1} />
+        <LeaderBoardUnit year={lastYear} month={lastMonth} />
       </TabsContent>
     </Tabs>
   );
