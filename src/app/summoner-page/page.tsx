7ad0df3d760ccbf472cd76844b0d9238d206d6e0
donghyun kim <donghyun1998@naver.com>
2025-01-04 18:08:40 +0900

    [Bugfix]: 없는 아이디 검색 navigate 후 새로 아이디 검색하면 맛 가는 문제 (#88)
    * fix: errorboundary에 key값 추가
    
    * fix: useSearchParams suspense내부에서 사용하게수정

--- src/app/summoner-page/page.tsx
+++ src/app/summoner-page/page.tsx
@@ -2,24 +2,35 @@
 
 import { Suspense } from "react";
 
+import { useSearchParams } from "next/navigation";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { Spinner } from "@/components/ui";
 
 import { ErrorPage, SummonerContent } from "./_components";
 
+const ErrorBoundaryWithKey = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const tag = searchParams.get("tag");
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorPage} key={`${id}-${tag}`}>
+      <SummonerContent />
+    </ErrorBoundary>
+  );
+};
+
 export default function SummonerPage() {
   return (
-    <ErrorBoundary FallbackComponent={ErrorPage}>
-      <Suspense
-        fallback={
-          <div className="flex h-96 w-full items-center justify-center">
-            <Spinner />
-          </div>
-        }
-      >
-        <SummonerContent />
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense
+      fallback={
+        <div className="flex h-96 w-full items-center justify-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <ErrorBoundaryWithKey />
+    </Suspense>
   );
 }
