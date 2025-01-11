ddce3280c6c5d0958fec0f7ab90f360410c193ae
donghyun <donghyun1998@naver.com>
2025-01-11 15:42:15 +0900

    fix: 첫 로딩에만 JandiBox안보이게 isLoading으로 변경

--- src/app/summoner-page/_components/summoner-content.tsx
+++ src/app/summoner-page/_components/summoner-content.tsx
@@ -12,12 +12,12 @@ export const SummonerContent = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id") ?? "";
   const tag = searchParams.get("tag") ?? "";
-  const { error, isFetching } = useQuery({
+  const { error, isLoading } = useQuery({
     queryKey: [QUERY_KEY.userInfo, { id, tag }],
     queryFn: fetchUserInfo,
   });
 
-  if (error && !isFetching) {
+  if (error) {
     throw error;
   }
 
@@ -25,7 +25,7 @@ export const SummonerContent = () => {
     <>
       <UserInfoBox id={id} tag={tag} />
       <TierBox id={id} tag={tag} />
-      {!isFetching && <JandiBox />}
+      {!isLoading && <JandiBox />}
     </>
   );
 };
